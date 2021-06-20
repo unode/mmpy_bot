f42d42e7122c3a9ea86b2d8cb8c008dd58389b34
Renato Alves <alves.rjc@gmail.com>
2021-06-20 19:55:38 +0200

    Remove unused create_message function
    (cherry picked from commit f64fe9978d1fbfda950a498525809cb43c0f0669)

--- tests/unit_tests/plugin_manager_test.py
+++ tests/unit_tests/plugin_manager_test.py
@@ -112,52 +112,6 @@ class TestPlugin:
         ]
 
 
-def create_message(
-    text="hello",
-    mentions=["qmw86q7qsjriura9jos75i4why"],
-    channel_type="O",
-    sender_name="betty",
-):
-    return Message(
-        {
-            "event": "posted",
-            "data": {
-                "channel_display_name": "Off-Topic",
-                "channel_name": "off-topic",
-                "channel_type": channel_type,
-                "mentions": mentions,
-                "post": {
-                    "id": "wqpuawcw3iym3pq63s5xi1776r",
-                    "create_at": 1533085458236,
-                    "update_at": 1533085458236,
-                    "edit_at": 0,
-                    "delete_at": 0,
-                    "is_pinned": "False",
-                    "user_id": "131gkd5thbdxiq141b3514bgjh",
-                    "channel_id": "4fgt3n51f7ftpff91gk1iy1zow",
-                    "root_id": "",
-                    "parent_id": "",
-                    "original_id": "",
-                    "message": text,
-                    "type": "",
-                    "props": {},
-                    "hashtags": "",
-                    "pending_post_id": "",
-                },
-                "sender_name": sender_name,
-                "team_id": "au64gza3iint3r31e7ewbrrasw",
-            },
-            "broadcast": {
-                "omit_users": "None",
-                "user_id": "",
-                "channel_id": "4fgt3n51f7ftpff91gk1iy1zow",
-                "team_id": "",
-            },
-            "seq": 29,
-        }
-    )
-
-
 class TestPluginManager:
     def setup_method(self):
         self.p = FakePlugin()
